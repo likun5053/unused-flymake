f91e9ab88e5da2d8ab4d7a5dead94f35e1a11257
kwinter <likun5053@gmail.com>
2011-05-25 23:01:50 +0800

    1.add sudo and force options.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 BIN_DIR=/usr/local/bin
 sudo apt-get install python-pip
 sudo pip install pylint pep8 pyflakes
-ln -s epylint ${BIN_DIR}/epylint 
-ln -s pycheckers ${BIN_DIR}/pycheckers 
+sudo ln -sf epylint ${BIN_DIR}/epylint 
+sudo ln -sf pycheckers ${BIN_DIR}/pycheckers 
  
